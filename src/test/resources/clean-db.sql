018871bc5a4e882190c874243d0bb4ccbc3c09ab
Alejandro Coves Bolaños <alejandrocoves2003@gmail.com>
2025-05-23 12:18:29 +0200

    Eighth test - Service methods for adding and retrieving users to a team
    Update clean-db.sql

--- src/test/resources/clean-db.sql
+++ src/test/resources/clean-db.sql
@@ -1,8 +1,4 @@
--- Option 1: Cascade the delete
-DELETE FROM equipo_usuario; -- Clear child table first
-DELETE FROM usuarios;       -- Then clear parent table
-
--- Option 2: Disable foreign key checks temporarily
-SET REFERENTIAL_INTEGRITY FALSE;
-DELETE FROM usuarios;       -- Perform the delete
-SET REFERENTIAL_INTEGRITY TRUE; -- Re-enable foreign key checks
+DELETE FROM equipo_usuario;
+DELETE FROM tareas;
+DELETE FROM equipos;
+DELETE FROM usuarios;
